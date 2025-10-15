cc63636ac7f9364bf705983ec310ca6d60cdffa6
Elonatech's Projects <elonatech.official@gmail.com>
2025-10-15 16:22:03 +0100

    Second commit

--- src/components/blog.js
+++ src/components/blog.js
@@ -12,21 +12,21 @@ export default function Blog() {
 
         <div>
             {/* First Card */}
-            <div className="flex flex-row bg-white">
+            <div className="flex flex-row bg-white gap-3">
                 <Image src="/images/woman-holding-a-laptop.png" alt="woman-holding-a-laptop" width={600} height={400}/>
-                <div className="flex flex-col text-start">
-                    <a href="" className="py-2 bg-[#e4e4e4] rounded-xl max-w-fit">Uncategorized</a>
-                    <a className="font-bold hover:text-blue-400 cursor-pointer ">Empowering the Next Generation of Women in Tech</a>
-                    <div className="flex flex-row gap-2 text-[12px]">
-                        <div><a>Admin Speedlink</a> . </div>
-                        <div><a>April 21, 2025</a> . </div>
-                        <div><a>No comments</a></div>
-                    </div>
-                    <div>
-                        <p className="text-sm text-gray-500">
-                        In a world increasingly deiven by technology, the voices shaping innovation must be as diverse as the people it serves. Yet, across ...
-                        </p>
-                    </div>
+                <div className="flex flex-col text-start pr-2">
+                  <a href="" className="py-2 bg-[#e4e4e4] rounded-2xl max-w-fit">Uncategorized</a>
+                  <a className="font-bold hover:text-blue-400 cursor-pointer ">Empowering the Next Generation of Women in Tech</a>
+                  <div className="flex flex-row gap-2 text-[12px]">
+                      <div><a>Admin Speedlink</a> . </div>
+                      <div><a>April 21, 2025</a> . </div>
+                      <div><a>No comments</a></div>
+                  </div>
+                  <div>
+                      <p className="text-sm text-gray-500">
+                      In a world increasingly deiven by technology, the voices shaping innovation must be as diverse as the people it serves. Yet, across ...
+                      </p>
+                  </div>
                 </div>
             </div>
         </div>
